63c5dfbe1548d9ce1308b6fdade41b1082523ba7
Avrou <avrouonline@gmail.com>
2023-01-10 23:03:30 -0800

    file

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -6,6 +6,9 @@ module.exports = async function(deployer) {
   await deployer.deploy(DaiTokenMock);
   const tokenMock = await DaiTokenMock.deployed()
   // Mint 1,000 Dai Tokens for the deployer
+  //comment
+    //comment
+
   await tokenMock.mint(
     '0xF3CEf9a118d483827880d2BA1379db1822aE2b6B',
     '1000000000000000000000'
